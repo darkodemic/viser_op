7eb050b9dfb27e5792ce5c53596fc60f56ebcf5b
Darko Demić <mail@darkodemic.com>
2017-03-17 09:16:22 +0100

    created vezba2_zadatak5

---
+++ vezba2_zadatak5.c
@@ -0,0 +1,21 @@
+/*
+Vezba: 2
+Zadatak: 5
+Dev: Darko Demic
+Built using: GCC 7.0 on Arch Linux
+*/
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[]) {
+    
+    printf("Enter A and B: ");
+    int a = 0; int b = 0; //Initialised them for safety reasons
+    scanf("%d %d", &a, &b); //We accept them as ints
+    //Added casting so operation would be performed as float not as int.
+    //Some optimization toolchains perform operation based on member types on final type
+    float c = ((float)(a) + b) / 2;
+    printf("Arithmetic mean:\nAs real number: %f\nAs Integer: %d",c, (int)(c));
+    
+    return 0;
+}
