e26e66dbbd4be766e4bbe2def9e3e7af9c9717ea
Darko Demić <mail@darkodemic.com>
2017-03-17 09:19:13 +0100

    added standard and changed to cpp

--- vezba2_zadatak5.cpp
+++ vezba2_zadatak5.cpp
@@ -3,6 +3,7 @@ Vezba: 2
 Zadatak: 5
 Dev: Darko Demic
 Built using: GCC 7.0 on Arch Linux
+Standard: C++ 17
 */
 
 #include <stdio.h>
@@ -13,7 +14,7 @@ int main(int argc, const char *argv[]) {
     int a = 0; int b = 0; //Initialised them for safety reasons
     scanf("%d %d", &a, &b); //We accept them as ints
     //Added casting so operation would be performed as float not as int.
-    //Some optimization toolchains perform operation based on member types on final type
+    //Some optimization toolchains perform operation based on member types not final type
     float c = ((float)(a) + b) / 2;
     printf("Arithmetic mean:\nAs real number: %f\nAs Integer: %d",c, (int)(c));
     
