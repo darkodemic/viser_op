d550632f07e3546230c2905cf33bbace9c63f633
Darko Demić <mail@darkodemic.com>
2017-03-17 09:26:01 +0100

    Create vezba3_zadatak6

---
+++ vezba3_zadatak6.cpp
@@ -0,0 +1,31 @@
+/*
+Vezba: 3
+Zadatak: 6
+Dev: Darko Demic
+Built using: GCC 7.0 on Arch Linux
+Standard: C++ 17
+*/
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[]) {
+    
+    printf("Enter X: "); float x = 0; scanf("%f",&x);
+    printf("Enter Y: "); float y = 0; scanf("%f",&y);
+    printf("Enter Z: "); float z = 0; scanf("%f",&z);
+    
+    if ( x >= y && y >= z) {
+        x *= 2; 
+        y *= 2;
+        z *= 2;
+    }
+    else {
+        x *= -1;
+        y *= -1;
+        z *= -1;
+    }
+    
+    printf("X: %f\nY: %f\nZ: %f", x, y, z);
+    
+    return 0;
+}
