1b13a754fe99edca6d4c003a3d3ffcb5d4961349
Darko Demić <mail@darkodemic.com>
2017-03-18 12:49:52 +0100

    update this

--- vezba8_zadatak4.cpp
+++ vezba8_zadatak4.cpp
@@ -12,43 +12,45 @@ Standard: C++ 17
 #define MAX_ARR_LENGTH 20
 
 int main(int argc, const char *argv[]) {
-
 	int arr[MAX_ARR_LENGTH];
+	int i, j, num, temp;
+	num = 0;
+	printf("\nEnter no of array elements: ");
+	scanf("%d", &num);
 
-	printf("Enter array length: ");
-	int arrLength = 0;
-	scanf("%d", &arrLength);
-	if (arrLength == 0) {
+	if (num == 0) {
 		printf("Error. Enter a valid length.");
 		exit(1);
 	}
-	else if (arrLength > MAX_ARR_LENGTH) {
+
+	if (num > MAX_ARR_LENGTH) {
 		printf("Maximum array length exceeded. Exiting.\n");
-		exit(2);
+		exit(1);
 	}
 
-	for (int i = 0; i < arrLength; i++) {
-		printf("Array element %d: ", i + 1);
+	//Read elements in an array
+	for (i = 0; i < num; i++) {
+		printf("Array element %d : ", i + 1);
 		scanf("%d", &arr[i]);
 	}
-	
-	int i = 0;       // i will be pointing to first element
-	int j = i - 1;   // j will Point to last Element
-	int temp = 0;
+
+	j = i - 1;   // j will Point to last Element
+	i = 0;       // i will be pointing to first element
+	temp = 0;
 
 	while (i < j) {
 		temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
-		i++;j--;          
+		i++;             // increment i
+		j--;          // decrement j
 	}
 
 	//Print out the Result
-	printf("\nResult after reversal: \n");
-	for (i = 0; i < arrLength; i++) {
-		printf("Array element %d: %d\n", i+1, arr[i]);
+	printf("\nResult after reversal : \n");
+	for (i = 0; i < num; i++) {
+		printf("Array element %d: %d\n", i + 1, arr[i]);
 	}
 
-	system("pause");
-	return 0;
+	return (0);
 }
