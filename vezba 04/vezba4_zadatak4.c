8e654003313ad946952f789e8b0ea72454f63dbd
Darko Demić <mail@darkodemic.com>
2017-03-19 21:53:17 +0100

    added a bit of formating

--- vezba 04/vezba4_zadatak4.c
+++ vezba 04/vezba4_zadatak4.c
@@ -10,21 +10,22 @@ Flags: -Wextra, -Wall, -Wfloat-equal, -O3, -ftrapv
 #include <stdio.h>
 
 int main(int argc, const char *argv[]) {
-    
-    int temp;
-    int max = 0; //0-ed so we can compare it
-    int min = 0; //0-ed so we can compare it
-    printf("Enter array of numbers, press enter after each number: \n");
-    while (scanf("%d",&temp), temp != 0) { //First arguments gets executed second evaluated
-        if (temp > max){
-            max = temp;    
-        }
-        else if (temp < min){
-            min = temp;
-        }
-    }
-    
-    printf("MAX: %d\nMIN: %d",max,min);
-    
-    return 0;
+
+	int temp;
+	int max = 0; //0-ed so we can compare it
+	int min = 0; //0-ed so we can compare it
+	printf("Enter array of numbers (press enter after each number | 0 Exists): \n"
+		"-----------------------------------------------------------------\n");
+	while (scanf("%d", &temp), temp != 0) { //First arguments gets executed second evaluated
+		if (temp > max) {
+			max = temp;
+		}
+		else if (temp < min) {
+			min = temp;
+		}
+	}
+
+	printf("MAX: %d\nMIN: %d\n", max, min);
+	
+	return 0;
 }
