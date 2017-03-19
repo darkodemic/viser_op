37adc4356336416fbd4e9e87608b44f535cfa60d
Darko Demić <mail@darkodemic.com>
2017-03-19 22:20:20 +0100

    optimized readCli

--- vezba 07/vezba7_zadatak4.c
+++ vezba 07/vezba7_zadatak4.c
@@ -30,7 +30,7 @@ char *readCli(int* bufferSize) {
 	enterEater = getchar();
 	char c; int i = 0;
 	while ((c = fgetc(stdin)) != '\n') {
-		if ((i % 30) == 0) {
+		if (((i % 30) == 0) && i != 0) {
 			buffer = (char*)realloc(buffer, (size_b * 2) * sizeof(char));
 			if (buffer == NULL) { exit(3); }
 		}
